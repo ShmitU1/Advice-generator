4c8b92d8f7a5fe51f77c91614cc759f393c9c31d
Łukasz <szmitu959@gmail.com>
2022-04-02 10:41:03 +0200

    Update SEO & functionality

--- app.js
+++ app.js
@@ -6,23 +6,29 @@ const diceBtn = document.querySelector('.btn')
 // get data form api advice side and insert into paragraphs
 async function getData() {
     try {
+        diceBtn.classList.add('disableBtn')
         const response = await fetch('https://api.adviceslip.com/advice')
         const data = await response.json()
+        generateAdvice(data);
+    }
+    catch(err) {
+        console.log('Error: ' + err.message);
+    }
+}
+// call getdata function
+getData()
 
-        
-        const {slip: {id, advice}} = data;
-        adviceNumber.innerText = `Advice #${id}`
-        adviceText.innerText = `"${advice}"`
-
+// Generate advice content on the page
+function generateAdvice(data) {
+    const {slip: {id, advice}} = data;
+    adviceNumber.innerText = `Advice #${id}`
+    adviceText.innerText = `"${advice}"`
+    
+    setTimeout(() => {
+        diceBtn.classList.remove('disableBtn')
         diceBtn.addEventListener('click', () => {
             document.location.reload(true)
         })
-    }
-    catch(err) {
-        console.log('Error ' + err.message);
-    }
+    }, 1000)
 }
-
-// call getdata function
-getData()
 
